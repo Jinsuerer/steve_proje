219196235cdae86dc5a38287626315621b16c2ea
Jinsuerer <h.jinsuerer@icloud.com>
2023-03-04 16:00:44 +0100

    Seite

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import People from "./pages/People";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/#" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
